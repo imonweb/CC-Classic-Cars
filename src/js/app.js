d081147b50f2815b0f4eab41b9e9b0b15649fd98
imonweb <contact@imonweb.co.uk>
2021-11-07 21:50:15 +0000

    section 4

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,15 @@ document.querySelectorAll('.wrapper').forEach((item) => {
   })
   })
 })
+
+/*  Video Gallery */
+const videos = document.querySelectorAll('.video');
+
+videos.forEach((video) => {
+  video.addEventListener('mouseover', () => {
+    video.play();
+  })
+  video.addEventListener('mouseout', () => {
+    video.pause();
+  })
+})
