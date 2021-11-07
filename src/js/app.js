35373efee620c136f6929a51f4400264042e3901
imonweb <contact@imonweb.co.uk>
2021-11-07 13:17:46 +0000

    section 1

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,8 @@ document.querySelector('.menu').addEventListener('click', () => {
 
 document.querySelectorAll('.wrapper').forEach((item) => {
   item.addEventListener('click', () => {
-     document.querySelectorAll('.target').forEach((item) => {
-    item.classList.toggle('remove');
+    document.querySelectorAll('.target').forEach((item) => {
+      item.classList.remove('change');
   })
   })
 })
